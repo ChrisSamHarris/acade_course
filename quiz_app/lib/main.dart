7a46b0262906c5ffb7293ea64e108183584844a0
christopher-harris_msmg <christopher.harris@moneysupermarket.com>
2025-02-03 19:14:35 +0000

    Started work on quiz app home page

---
+++ quiz_app/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/gradientContainer.dart';
+
+const colorStart = Color.fromARGB(255, 155, 213, 230);
+const colorEnd = Color.fromARGB(255, 51, 143, 229);
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(colorStart:colorStart, colorEnd:colorEnd),
+      ),
+    ),
+  );
+}
